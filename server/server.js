c54ba945f212bdf4a07852e755e0087d92aaf2fd
Shivraj2615 <shivdilipjagdale@gmail.com>
2026-02-22 02:16:56 +0530

    Removed vercel.json

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
